cb45d600029ca54bd03030c5e04dc94d0061fe42
jgrynczewski <jgrynczewski@gmail.com>
2023-01-16 14:46:47 +0100

    types and operators 1

---
+++ scripts/day_1/02_literals.py
@@ -0,0 +1,24 @@
+# całkowitoliczbowy (integer)
+print(3)
+print(10)
+print(-55)
+
+# zmiennoprzecinkowe (float)
+print(2.3)
+print(5.3412)
+print(4.)
+print(.23)
+print(3.2e3)
+
+# napisowe (string)
+print("ala ma kota")
+print("Napis pierwsz")
+print('Drugi napis')
+print("""Trzeci napis""")
+
+# logiczne (boolean)
+print(False)
+print(True)
+
+# pusty (none)
+print(None)
---
+++ scripts/day_1/03_operators.py
@@ -0,0 +1,30 @@
+# operatory arytmetyczne
+
+# TYP CAŁKOWITOLICZBOWY
+# operator dodawania
+print(3 + 4)
+
+# operator odejmowania
+print(4 - 2)
+
+# operator mnożenia
+print(2 * 5)
+
+# operator dzielenia
+print(6 / 2)
+
+# operator dzielenie całkowitoliczbowe
+print(5 // 2)
+
+# operator reszty z dzieleni
+print(5 % 2)
+
+# operator potęgowania
+print(3 ** 2)
+
+# TYP ZMIENNOPRZECNIKOWY
+# Identycznie z operatormi arytemtycznymi dla float
+print(6.2 // 3.4)
+print(6.2 % 3.4)
+print(0.1+0.1+0.1)
+
