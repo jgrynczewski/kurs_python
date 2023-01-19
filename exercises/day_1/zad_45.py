ee4f40160595d7b8b2533fe480438e6b8374b78a
jgrynczewski <jgrynczewski@gmail.com>
2023-01-19 07:02:20 +0100

    day4 fix

--- exercises/day_1/zad_45.py
+++ exercises/day_1/zad_45.py
@@ -1,4 +1,4 @@
 # Rekurencja
 
 # Napisz funkcję, która obliczy silnie dla zadanego parametru.
-# Następnie napiszą tę funkcję w wersji rekurencyjnej.
+# Następnie napisz taką funkcję w wersji rekurencyjnej.
