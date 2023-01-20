38c23227fca41aec620e4787776d57566b8684b2
jgrynczewski <jgrynczewski@gmail.com>
2023-01-20 09:03:48 +0100

    day5 materialy

---
+++ exercises/day_1/zad_47.py
@@ -0,0 +1,6 @@
+# Napisz klasę Ulamek, która przechowuje ułamki postaci a/b.
+# Klasa powinna mieć dwa atrybuty - licznik i mianownik
+# oraz dwie metody:
+# - skroc (metoda wymaga obliczenia największego wspólnego
+# dzielnika, możesz skorzystać z funkcji gcd modułu math)
+# - mnoz (oblicza iloczyn dwoch ulamkow)
